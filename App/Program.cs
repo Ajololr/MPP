6fd0a50eada27590d8e430971ec3ab87f5a40bf8
Ilya Androsov <ilya_androsov@icloud.com>
2020-09-09 16:07:30 +0300

    feat: add aditional files

---
+++ App/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Tracer;
+using XmlSerializer = Tracer.XmlSerializer;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TracerImpl tracerImpl = new TracerImpl();
+            
+            Foo foo = new Foo(tracerImpl);
+            foo.MyMethod();
+            foo.AnotherMethod();
+            
+            Other other = new Other(tracerImpl);
+            Thread InstanceCaller = new Thread(
+                new ThreadStart(other.OtherMethod));
+            InstanceCaller.Start();
+            
+            Bar bar = new Bar(tracerImpl);
+            bar.InnerMethod();
+
+            InstanceCaller.Join();
+                
+            TraceResult traceResult = tracerImpl.GetTraceResult();
+            
+            ISerializer<TraceResult> serializer = new XmlSerializer();
+            serializer.stringify(traceResult);
+            serializer.writeToConsole();
+            serializer.saveToFile();
+        }
+    }
+    
+    public class Foo
+    {
+        private Bar _bar;
+        private ITracer _tracer;
+
+        internal Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+    
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+            
+            _bar.InnerMethod();
+
+            _tracer.StopTrace();
+        }
+        
+        public void AnotherMethod()
+        {
+            _tracer.StartTrace();
+            
+            Other other =new Other(_tracer);
+            other.OtherMethod();
+
+            _tracer.StopTrace();
+        }
+    }
+
+    public class Bar
+    {
+        private ITracer _tracer;
+
+        internal Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+    
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                
+            }
+            
+            _tracer.StopTrace();
+        }
+    }
+    
+    public class Other
+    {
+        private ITracer _tracer;
+
+        internal Other(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+    
+        public void OtherMethod()
+        {
+            _tracer.StartTrace();
+
+            Thread.Sleep(20);
+            
+            _tracer.StopTrace();
+        }
+    }
+}
---
+++ Tracer/TracerImpl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Tracer
+{
+    
+    public class TracerImpl : ITracer
+    {
+        public TraceResult Result = new TraceResult();
+        private Dictionary<int, Stack<MethodInfo>> _methodsListMap = new Dictionary<int, Stack<MethodInfo>>();
+        private Stack<Stopwatch> _watch = new Stack<Stopwatch>();
+        
+        public void StartTrace()
+        {
+            string className = "";
+            string methodName = "";
+            int threadId = -1;
+
+            GetMethodData(out methodName, out className, out threadId);
+            MethodInfo methodInfo = new MethodInfo(methodName, className);
+
+            ThreadInfo threadInfo = new ThreadInfo(threadId);
+
+            LinkedListNode<ThreadInfo> node = Result.Threads.Find(threadInfo);
+            
+            if (node == null)
+            {
+                node = Result.Threads.AddLast(threadInfo);
+                node.Value.Methods.AddLast(methodInfo);
+                _methodsListMap[threadId] = new Stack<MethodInfo>();
+            }
+            else
+            {
+                if (_methodsListMap[threadId].Count != 0)
+                {
+                    _methodsListMap[threadId].Peek().Methods.AddLast(methodInfo);
+                }
+                else
+                {
+                    node.Value.Methods.AddLast(methodInfo);
+                }
+            }
+            
+            _methodsListMap[threadId].Push(methodInfo);
+            _watch.Push(System.Diagnostics.Stopwatch.StartNew());
+        }
+
+        public void StopTrace()
+        {
+            _watch.Peek().Stop();
+            Stopwatch stopwatch = _watch.Pop();
+            long elapsedMs = stopwatch.ElapsedMilliseconds;
+
+            int threadId = GetThreadId();
+            _methodsListMap[threadId].Pop().ElapsedMs = elapsedMs;
+            
+            ThreadInfo threadInfo = new ThreadInfo(threadId);
+            LinkedListNode<ThreadInfo> node = Result.Threads.Find(threadInfo);
+            node.Value.ElapsedMs += elapsedMs;
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return Result;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void GetMethodData(out string methodName, out string className, out int threadId)
+        {
+            const int index = 2;
+            
+            var st = new StackTrace();
+            var sf = st.GetFrame(index);
+            
+            MethodBase method = sf.GetMethod();
+            
+            className = method.ReflectedType.Name;
+            methodName = method.Name;
+            threadId = GetThreadId();
+        }
+
+        private int GetThreadId()
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+}
