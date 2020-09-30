8fe8f60cd1aac147884a893c4652db8065cb6f6a
Ilya Androsov <ilya_androsov@icloud.com>
2020-09-30 17:00:05 +0300

    feat: add lab-3

---
+++ lab-3/Assembly Lib/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Assembly_Lib
+{
+    public class AssemblyLib
+    {
+        public static AssemblyInfo GetAssemblyInfo(string path)
+        {
+            AssemblyInfo assemblyInfo = new AssemblyInfo();
+            Assembly assembly = Assembly.LoadFile(path);
+            foreach (Type type in assembly.GetTypes())
+            {
+                string currentNamespace = type.Namespace;
+                if (!assemblyInfo.NamespaceInfos.ContainsKey(currentNamespace))
+                {
+                    assemblyInfo.NamespaceInfos.Add(currentNamespace, new NamespaceInfo());
+                }
+                NamespaceInfo namespaceInfo = assemblyInfo.NamespaceInfos[currentNamespace];
+
+                namespaceInfo.DataTypeInfos.AddLast(new DataTypeInfo(type.GetFields(), type.GetProperties(),
+                    type.GetMethods()));
+            }
+
+            return assemblyInfo;
+        } 
+    }
+
+    public class AssemblyInfo
+    {
+        public Dictionary<string, NamespaceInfo> NamespaceInfos = new Dictionary<string, NamespaceInfo>();
+    }
+
+    public class NamespaceInfo
+    {
+        public LinkedList<DataTypeInfo> DataTypeInfos = new LinkedList<DataTypeInfo>();
+    }
+
+    public class DataTypeInfo
+    {
+        public FieldInfo[] FieldInfos;
+        public PropertyInfo[] PropertyInfos;
+        public MethodInfo[] MethodInfos;
+
+        public DataTypeInfo(FieldInfo[] fieldInfos, PropertyInfo[] propertyInfos, MethodInfo[] methodInfos)
+        {
+            FieldInfos = fieldInfos;
+            PropertyInfos = propertyInfos;
+            MethodInfos = methodInfos;
+        }
+    }
+}
---
+++ lab-3/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Assembly_Lib;
+using Microsoft.Win32;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Dll files(*.dll)|*.dll";
+            openFileDialog.ShowDialog();
+            string filename = openFileDialog.FileName;
+            if (filename == "") return;
+            AssemblyInfo assemblyInfo = AssemblyLib.GetAssemblyInfo(filename);
+            Console.WriteLine(assemblyInfo);
+        }
+        
+    }
+}
