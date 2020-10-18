1be5c2ad50bbad9d10182280a9d709e183092ab5
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-18 18:54:13 +0300

    feat: add code parser

---
+++ lab-3/ConsoleApp1/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class SyncEvents
+{
+    public SyncEvents()
+    {
+
+        _newItemEvent = new AutoResetEvent(false);
+        _exitThreadEvent = new ManualResetEvent(false);
+        _eventArray = new WaitHandle[2];
+        _eventArray[0] = _newItemEvent;
+        _eventArray[1] = _exitThreadEvent;
+    }
+
+    public EventWaitHandle ExitThreadEvent
+    {
+        get { return _exitThreadEvent; }
+    }
+    public EventWaitHandle NewItemEvent
+    {
+        get { return _newItemEvent; }
+    }
+    public WaitHandle[] EventArray
+    {
+        get { return _eventArray; }
+    }
+
+    private EventWaitHandle _newItemEvent;
+    private EventWaitHandle _exitThreadEvent;
+    private WaitHandle[] _eventArray;
+}
+public class Producer 
+{
+    public Producer(Queue<int> q, SyncEvents e)
+    {
+        _queue = q;
+        _syncEvents = e;
+    }
+    // Producer.ThreadRun
+    public void ThreadRun()
+    {
+        int count = 0;
+        Random r = new Random();
+        while (count < 1_000_000)
+        {
+            lock (((ICollection)_queue).SyncRoot)
+            {
+                    _queue.Enqueue(r.Next(0,100));
+                    _syncEvents.NewItemEvent.Set();
+                    count++;
+            }
+        }
+        Console.WriteLine("Producer thread: produced {0} items", count);
+        _syncEvents.ExitThreadEvent.Set();
+    }
+    private Queue<int> _queue;
+    private SyncEvents _syncEvents;
+}
+
+public class Consumer
+{
+    public Consumer(Queue<int> q, SyncEvents e)
+    {
+        _queue = q;
+        _syncEvents = e;
+    }
+    // Consumer.ThreadRun
+    public void ThreadRun()
+    {
+        int count = 0;
+        while (WaitHandle.WaitAny(_syncEvents.EventArray) != 1 || _queue.Count != 0)
+        {
+            lock (((ICollection)_queue).SyncRoot)
+            {
+                int item = _queue.Dequeue();
+            }
+            count++;
+        } 
+        Console.WriteLine("Consumer Thread: consumed {0} items", count);
+    }
+    private Queue<int> _queue;
+    private SyncEvents _syncEvents;
+}
+
+public class ThreadSyncSample
+{
+    private static void ShowQueueContents(Queue<int> q)
+    {
+        lock (((ICollection)q).SyncRoot)
+        {
+            foreach (int item in q)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Queue<int> queue = new Queue<int>();
+        SyncEvents syncEvents = new SyncEvents();
+
+        Console.WriteLine("Configuring worker threads...");
+        Producer producer = new Producer(queue, syncEvents);
+        Consumer consumer = new Consumer(queue, syncEvents);
+        Thread producerThread = new Thread(producer.ThreadRun);
+        Thread consumerThread1 = new Thread(consumer.ThreadRun);
+        Thread consumerThread2 = new Thread(consumer.ThreadRun);
+        Thread consumerThread3 = new Thread(consumer.ThreadRun);
+        Thread consumerThread4 = new Thread(consumer.ThreadRun);
+
+        Console.WriteLine("Launching producer and consumers threads..."); 
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        producerThread.Start();
+        consumerThread1.Start();
+        consumerThread2.Start();
+        consumerThread3.Start();
+        consumerThread4.Start();
+
+        // for (int i=0; i<4; i++)
+        // {
+        //     Thread.Sleep(2500);
+        //     ShowQueueContents(queue);
+        // }
+        //
+        // Console.WriteLine("Signaling threads to terminate...");
+        // syncEvents.ExitThreadEvent.Set();
+
+        producerThread.Join();
+        consumerThread1.Join();
+        consumerThread2.Join();
+        consumerThread3.Join();
+        consumerThread4.Join();
+        
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+    }
+
+}
---
+++ lab-4/TestsClassGenerator/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestsClassGenerator
+{
+    public class TestClassGenerator
+    {
+        public string GenerateTestClasses(string sourceCode)
+        {
+            string result = "";
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            List<MethodDeclarationSyntax> methods = syntaxTree.GetRoot().DescendantNodes()
+                .OfType<MethodDeclarationSyntax>().ToList().Where(method => method.Modifiers.ToList().Any(token => token.Text == "public")).ToList();
+            foreach(var method in methods)
+            { 
+                Console.WriteLine(method.Identifier);
+            }
+            return result;
+        }
+    }
+}
