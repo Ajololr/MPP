7357611767d827ddfc076ebe4ccf5bc3eb72276f
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-26 23:42:00 +0300

    feat: add MaxDegreeOfParallelism

--- lab-4/ConsoleApp/Program.cs
+++ lab-4/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks.Dataflow;
 using TestsClassGenerator;
@@ -6,9 +8,24 @@ using TestsClassGenerator;
 class Dataflow
 {
    private const string DestPath = "E:\\University\\СПП\\lab-4\\Output\\";
-   
+   private const int MaxDegreeOfParallelismLoad = 1;
+   private const int MaxDegreeOfParallelismGenerate = 1;
+   private const int MaxDegreeOfParallelismSave = 1;
+   private static string[] _input = new[] { 
+      "E:\\University\\СПП\\lab-2\\Faker\\Faker.cs", 
+      "E:\\University\\СПП\\lab-1\\Tracer\\TracerImpl.cs",
+      "E:\\University\\СПП\\lab-1\\Tracer\\JsonSerializer.cs",
+      "E:\\University\\СПП\\lab-1\\Tracer\\XmlSerializer.cs",
+      "E:\\University\\СПП\\lab-1\\App\\Program.cs",
+      "E:\\University\\СПП\\lab-3\\WpfApp\\ApplicationViewModel.cs",
+      "E:\\University\\СПП\\lab-3\\Assembly Lib\\Class1.cs"
+   };
+
    static void Main()
    {
+      var sw = new Stopwatch();
+      sw.Start();
+      
       var loadSourceFileToMemory = new TransformBlock<string, string>(async path =>
       {
          Console.WriteLine("Loading to memory '{0}'...", path);
@@ -17,24 +34,34 @@ class Dataflow
          {
             return await SourceReader.ReadToEndAsync();
          }
+      }, new ExecutionDataflowBlockOptions
+      {
+         MaxDegreeOfParallelism = MaxDegreeOfParallelismLoad
       });
 
-      var generateTestClass = new TransformBlock<string, string>(async text =>
+      var generateTestClass = new TransformManyBlock<string, TestClass>(async text =>
       {
          Console.WriteLine("Generating test class...");
          
          TestClassGenerator classGenerator = new TestClassGenerator();
-         
+
          return classGenerator.GenerateTestClasses(text);
+      }, new ExecutionDataflowBlockOptions
+      {
+         MaxDegreeOfParallelism = MaxDegreeOfParallelismGenerate
       });
 
-      var saveTestClassToFile = new ActionBlock<string>(async text =>
+      var saveTestClassToFile = new ActionBlock<TestClass>(async testClass =>
       {
-         using (StreamWriter DestinationWriter = File.CreateText(DestPath + "out.cs"))
+         using (StreamWriter DestinationWriter = File.CreateText(DestPath + testClass.FileName))
          {
-            Console.WriteLine("Saving class to file...");
-            await DestinationWriter.WriteAsync(text);
+            Console.WriteLine("Saving '{0}' class to file...", testClass.FileName);
+            await DestinationWriter.WriteAsync(testClass.Source);
+            Console.WriteLine("Saved!");
          }
+      }, new ExecutionDataflowBlockOptions
+      {
+         MaxDegreeOfParallelism = MaxDegreeOfParallelismSave
       });
 
       var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
@@ -42,11 +69,16 @@ class Dataflow
       loadSourceFileToMemory.LinkTo(generateTestClass, linkOptions);
       generateTestClass.LinkTo(saveTestClassToFile, linkOptions);
 
-      loadSourceFileToMemory.Post("E:\\University\\СПП\\lab-2\\Faker\\Faker.cs");
+      foreach (var path in _input)
+      {
+         loadSourceFileToMemory.Post(path);
+      }
 
       loadSourceFileToMemory.Complete();
 
       saveTestClassToFile.Completion.Wait();
+      sw.Stop();
+      Console.WriteLine(sw.ElapsedMilliseconds);
    }
 
    public void Foo()
